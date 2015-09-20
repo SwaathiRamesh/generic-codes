549d1cdee50d64d96ab4e8cc16c4a084fb915ed3
swat <swramesh92@gmail.com>
2015-09-20 16:03:54 -0400

    Counts who survives

--- josephus.cpp
+++ josephus.cpp
@@ -1,1 +1,20 @@
 // code for solution to Josephus problem
+#include<iostream>
+using namespace std;
+int T[10][10];
+int josephus(int n, int k)
+{
+	cout<<"\n\t in J("<<n<<","<<k<<")";
+	if( n==1) return 1;
+	int t= ((josephus(n-1,k)+k-1)%n+1);
+	cout<<"\n \t Returning "<<t<<" in J("<<n<<","<<k<<")";
+	return t;
+}
+int main()
+{
+	int n,k;
+	cin>>n>>k;
+	int i,j;
+	cout<<"\n The one that survives: "<<josephus(n,k);
+
+}
